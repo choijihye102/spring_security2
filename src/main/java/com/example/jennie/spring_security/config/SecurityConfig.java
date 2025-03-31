6a6e6b479d1a63b985861da6172141e50b3e4417
ChoiJiHye <stdy1227@gmail.com>
2025-03-31 11:27:04 +0900

    JWT 의존성 추가 JWT 비밀키, 만료시간 설정 SecurityConfig에 JWT 관련 필터 설정 JwtAuthenticationFilter 구현 JwtTokenProvider 구현

--- src/main/java/com/example/jennie/spring_security/config/SecurityConfig.java
+++ src/main/java/com/example/jennie/spring_security/config/SecurityConfig.java
@@ -1,55 +1,61 @@
 package com.example.jennie.spring_security.config;
 
+import com.example.jennie.spring_security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final UserDetailsService userDetailsService;
+   // private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     //SecurityFilterChain :  시큐리티에서 적용할 보안규칙들을 필터로 구현해놓음
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
              http
                  .csrf().disable() // CSRF 필터 끔
-                 .userDetailsService(userDetailsService)
+                // .userDetailsService(userDetailsService)
                  .authorizeRequests() // URL 기반 인가 설정
                     .antMatchers("/user/**").hasRole("USER")  // USER 권한 사용자만 접근 가능
                     .antMatchers("/admin/**").hasRole("ADMIN")
                     .antMatchers("/logout").authenticated() // 인증받은 사용자만 접근 가능
                     .antMatchers("/**","/member/**").permitAll() // 인증/인가 여부와 상관없이 접근 가능
                      .and()
-                 .formLogin()
-                     .loginPage("/member/login")    // 커스텀 로그인 페이지 경로
-                     //.loginProcessingUrl("/api/login")  // 로그인 처리 URL
-                     .usernameParameter("userid")   // 아이디 매개변수 지정
-                     .passwordParameter("passwd")   // 비밀번호 매개변수 지정
-                     .defaultSuccessUrl("/")    // 로그인 성공시 리다이렉트 URL
-                     .failureUrl("/login?error=true")    // 로그인 실패시 리다이렉트 URL
-                     .permitAll()
-                     .and()
-                 .logout()// 로그아웃 설정
-                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                     .logoutSuccessUrl("/") // 로그아웃 성공후 리다이렉트 될 URL
-                     .invalidateHttpSession(true) // 세션 무효화
-                     .deleteCookies("JSESSIONID") // JSESSIONID 쿠키 삭제
-                     .permitAll();
+                     .sessionManagement()   // JWT 인증을 위해 Stateless 설정
+                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+             // JWT 필터 설정
+             http.addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
+
+
+
+//                 .formLogin()
+//                     .loginPage("/member/login")    // 커스텀 로그인 페이지 경로
+//                     //.loginProcessingUrl("/api/login")  // 로그인 처리 URL
+//                     .usernameParameter("userid")   // 아이디 매개변수 지정
+//                     .passwordParameter("passwd")   // 비밀번호 매개변수 지정
+//                     .defaultSuccessUrl("/")    // 로그인 성공시 리다이렉트 URL
+//                     .failureUrl("/login?error=true")    // 로그인 실패시 리다이렉트 URL
+//                     .permitAll()
+//                     .and()
+//                 .logout()// 로그아웃 설정
+//                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                     .logoutSuccessUrl("/") // 로그아웃 성공후 리다이렉트 될 URL
+//                     .invalidateHttpSession(true) // 세션 무효화
+//                     .deleteCookies("JSESSIONID") // JSESSIONID 쿠키 삭제
+//                     .permitAll();
 
         return http.build();
     }
